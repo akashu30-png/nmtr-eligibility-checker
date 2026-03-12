abffd863bee924a428bbb923a0680284710dc9c8
akashu30-png <akashu30@gmail.com>
2026-03-12 11:21:14 +0000

    Add App component with EligibilityChecker

---
+++ nmtr-eligibility-app/ src/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import EligibilityChecker from "./components/EligibilityChecker.jsx";
+
+export default function App() {
+  return (
+    <div className="p-6">
+      <EligibilityChecker />
+    </div>
+  );
+}
